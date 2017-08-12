c5317d1ad36d310bd9296b47d45bae09029cef18
Piotrek Chmielowski <piotrek.chmielowski@callpage.io>
2017-08-12 20:41:42 +0200

    init

---
+++ DroidConfParser.js
@@ -0,0 +1,19 @@
+
+// Find talks
+var talks = document.getElementsByClassName('view-content')[0].children
+
+// Extract names
+var getName = talk => talk.getElementsByClassName('views-field views-field-title')[0].getElementsByClassName('field-content')[0].children[0].innerHTML;
+
+var getDate = talk => talk.getElementsByClassName('views-field views-field-field-session-datetime')[0].getElementsByClassName('field-content')[0].children[0].innerHTML.split(" ")[0];
+var getStartTime = talk => talk.getElementsByClassName('views-field views-field-field-session-datetime')[0].getElementsByClassName('field-content')[0].getElementsByClassName('date-display-start')[0].innerHTML;
+var getEndTime = talk => talk.getElementsByClassName('views-field views-field-field-session-datetime')[0].getElementsByClassName('field-content')[0].getElementsByClassName('date-display-end')[0].innerHTML;
+
+var getRoom = talk => talk.getElementsByClassName('views-field views-field-field-session-room')[0].getElementsByClassName('field-content')[0].children[0].innerHTML.split(" ")[1];
+
+
+
+var parseTalk = talk => ({ topic : getName(talk), date : getDate(talk), starts : getStartTime(talk), ends : getEndTime(talk), room : getRoom(talk) })
+
+
+JSON.stringify(Array.from(talks).map(parseTalk))
