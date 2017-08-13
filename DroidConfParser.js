f7daef60aa63c1902c52e6fa0d03a4a6ba51a7a0
Piotrek Chmielowski <piotrek.chmielowski@callpage.io>
2017-08-13 18:17:33 +0200

    Link to original site

--- DroidConfParser.js
+++ DroidConfParser.js
@@ -4,6 +4,7 @@ var talks = document.getElementsByClassName('view-content')[0].children
 
 // Extract names
 var getName = talk => talk.getElementsByClassName('views-field views-field-title')[0].getElementsByClassName('field-content')[0].children[0].innerHTML;
+var getLink = talk => talk.getElementsByClassName('views-field views-field-title')[0].getElementsByClassName('field-content')[0].children[0].href;
 
 var getDate = talk => talk.getElementsByClassName('views-field views-field-field-session-datetime')[0].getElementsByClassName('field-content')[0].children[0].innerHTML.split(" ")[0];
 var getStartTime = talk => talk.getElementsByClassName('views-field views-field-field-session-datetime')[0].getElementsByClassName('field-content')[0].getElementsByClassName('date-display-start')[0].innerHTML;
@@ -13,7 +14,7 @@ var getRoom = talk => talk.getElementsByClassName('views-field views-field-field
 
 
 
-var parseTalk = talk => ({ topic : getName(talk), date : getDate(talk), starts : getStartTime(talk), ends : getEndTime(talk), room : getRoom(talk) })
+var parseTalk = talk => ({ topic : getName(talk), date : getDate(talk), starts : getStartTime(talk), ends : getEndTime(talk), room : getRoom(talk), link : getLink(talk) })
 
 
 JSON.stringify(Array.from(talks).map(parseTalk))
